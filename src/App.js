2923c2c25a89409aa9c9d4a1b20935096f1af30d
Soyoun Kim <makeajourney@gmail.com>
2020-01-06 21:00:53 +0900

    url parameter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import Profile from "./Profile";
 
 function App() {
   return (
@@ -13,10 +14,17 @@ function App() {
         <li>
           <Link to="/about">Introduction</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert profile</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong profile</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 }
