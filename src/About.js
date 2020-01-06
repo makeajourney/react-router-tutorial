6677bcf65c0efdfcb86f6c024533b4a3ad8740a8
Soyoun Kim <makeajourney@gmail.com>
2020-01-06 21:21:12 +0900

    url query

--- src/About.js
+++ src/About.js
@@ -1,10 +1,16 @@
 import React from "react";
+import qs from "qs";
 
-function About() {
+function About({ location }) {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true
+  });
+  const showDetail = query.detail === "true";
   return (
     <div>
       <h1>Introduction</h1>
       <p>This is project of example for practicing react router principal.</p>
+      {showDetail && <p>setted detail true</p>}
     </div>
   );
 }
