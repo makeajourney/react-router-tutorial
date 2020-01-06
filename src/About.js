1e9c918ac2d117ae2b7117766ed9680ce928cc7d
Soyoun Kim <makeajourney@gmail.com>
2020-01-06 20:47:25 +0900

    Route component로 특정 주소에 컴포넌트 연결

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>Introduction</h1>
+      <p>This is project of example for practicing react router principal.</p>
+    </div>
+  );
+}
+
+export default About;
